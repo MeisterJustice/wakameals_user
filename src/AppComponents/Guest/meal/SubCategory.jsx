e2dfc3e36b22f10062a863bd7e0ce1bdd27ccb8a
Justice Eziefule <thechiefje@gmail.com>
2021-02-28 14:37:37 -0800

    fixes

--- src/AppComponents/Guest/meal/SubCategory.jsx
+++ src/AppComponents/Guest/meal/SubCategory.jsx
@@ -4,6 +4,9 @@ import Meal from "./Meal";
 const SubCategory = (props) => {
     return (
         <div>
+            {props.data.meals.length > 0 && (
+                <h6 className="cat-title">{props.data.name}</h6>
+            )}
             {props.data.meals.map((meal, index) => (
                 <Meal index={index} notifySuccess={props.notifySuccess} handleAddCart={props.handleAddCart} {...props} key={index} meal={meal} />
             ))}
